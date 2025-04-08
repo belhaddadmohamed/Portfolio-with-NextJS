a659e227c085edc60f69fa7c98e7897856aa80db
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2025-04-08 11:25:16 +0100

    Update front

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -32,7 +32,7 @@ const Banner = () => {
         <div className="flex items-center">
             <button data-dismiss-target="#sticky-banner" type="button" onClick={handleCloseBanner} className="shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
             <svg className="w-3 h-3" aria-hidden="true" xmlns="http:www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
                 <span className="sr-only">Close banner</span>
             </button>
--- app/sections/Portfolio/Portfolio.tsx
+++ app/sections/Portfolio/Portfolio.tsx
@@ -97,9 +97,9 @@ const Portfolio = () => {
               View case study
               <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
-                <path fill-rule="evenodd"
+                <path fillRule="evenodd"
                   d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </a>
           </div>
@@ -121,9 +121,9 @@ const Portfolio = () => {
               View case study
               <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
-                <path fill-rule="evenodd"
+                <path fillRule="evenodd"
                   d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </a>
           </div>
@@ -145,9 +145,9 @@ const Portfolio = () => {
               View case study
               <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
-                <path fill-rule="evenodd"
+                <path fillRule="evenodd"
                   d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </a>
           </div>
